876e09e61d8bdfa91b3e15b857c1791fc26d8986
Pedro-0101 <pedro.tiete@outlook.com>
2025-11-20 15:22:15 -0300

    feat: resolvido desaafio busca linear

--- src/searching/linearSearch.c
+++ src/searching/linearSearch.c
@@ -20,3 +20,49 @@
  * - Lento para Grandes Dados: Complexidade O(n). Se o array tiver 1 milhão de itens, pode precisar de 1 milhão de comparações.
  * - Ineficiente: Comparado à busca binária, é muito custoso para buscas repetidas em dados estáticos.
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+
+ #define ARRAY_SIZE 1000000
+
+int linearSearch(int array[], int size, int target) {
+ for(int i = 0; i<size; i++) {
+  if(array[i] == target){
+    return i;
+  }
+ }
+ return -1;
+}
+ 
+int main() {
+
+  // Alocação dinâmica para evitar Stack Overflow (4MB excede o limite padrão da stack)
+  int *array = (int*)malloc(ARRAY_SIZE * sizeof(int));
+  if (array == NULL) {
+      printf("Erro de alocacao de memoria\n");
+      return 1;
+  }
+  int target;
+  int i;
+
+  printf("Inciando a busca linear\n");
+
+  // Inicializa o array com números aleatórios
+  srand(time(NULL));
+  for (i = 0; i < ARRAY_SIZE; i++) {
+    array[i] = rand() % ARRAY_SIZE;
+  }
+
+  // Busca o número 42
+  target = 42;
+  int result = linearSearch(array, ARRAY_SIZE, target);
+
+  if (result != -1) {
+    printf("Elemento %d encontrado na posicao %d\n", target, result);
+  } else {
+    printf("Elemento %d nao encontrado\n", target);
+  }
+
+}
